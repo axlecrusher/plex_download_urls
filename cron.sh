a1a6f0c54141b9326cbc49860a59d847522fc00d
axlecrusher <axlecrusher@gmail.com>
2020-09-22 07:30:25 -0400

    Link updates for 09/22/2020

--- cron.sh
+++ cron.sh
@@ -3,13 +3,4 @@
 script_relative_path1=`dirname $0`
 echo $script_relative_path1
 cd $script_relative_path1
-exec &>> cron.log
-
-now=`date +'%m/%d/%Y'`
-
-git fetch
-git pull
-
-./Grab.sh
-git commit -a -m "Link updates for $now"
-#git push
+./automateUpdate.sh | tai64n > cron.log
