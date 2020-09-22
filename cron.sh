d0e40a27e61d17df08e44a6c69c348d8c243a93d
axlecrusher <axlecrusher@gmail.com>
2020-09-22 06:53:23 -0400

    add logging to scriptr

--- cron.sh
+++ cron.sh
@@ -1,5 +1,10 @@
 #!/bin/bash 
 
+script_relative_path1=`dirname $0`
+echo $script_relative_path1
+cd $script_relative_path1
+exec &>> cron.log
+
 now=`date +'%m/%d/%Y'`
 
 git fetch
@@ -7,4 +12,4 @@ git pull
 
 ./Grab.sh
 git commit -a -m "Link updates for $now"
-git push
+#git push
